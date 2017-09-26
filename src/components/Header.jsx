a7a700ebbeaf25cb198988c0ab7c63789bb45016
Nick Ginther <nginth@gmail.com>
2017-09-26 17:58:35 -0500

    add basic navigation drawer to Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,35 @@
 import React from "react";
 import AppBar from "material-ui/AppBar";
+import Drawer from "material-ui/Drawer";
+import Divider from "material-ui/Divider";
+import MenuItem from "material-ui/MenuItem";
 
-const Header = () => <AppBar title="Playlistr" />;
+export default class Header extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = { open: false };
+	}
 
-export default Header;
+	handleToggle = () => this.setState({ open: !this.state.open });
+
+	handleClose = () => this.setState({ open: false });
+
+	render() {
+		return (
+			<div>
+				<AppBar title="Playlistr" onLeftIconButtonTouchTap={this.handleToggle} />
+				<Drawer
+					docked={false}
+					width={200}
+					open={this.state.open}
+					onRequestChange={open => this.setState({ open })}
+				>
+					<MenuItem onClick={this.handleClose}>Menu Item</MenuItem>
+					<Divider />
+					<MenuItem onClick={this.handleClose}>Menu Item 2</MenuItem>
+					<Divider />
+				</Drawer>
+			</div>
+		);
+	}
+}
