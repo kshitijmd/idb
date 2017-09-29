0560fcc882f5944b596814f72ef5053e5316491b
Taylor Kline <taylor.kline@streamingfreebies.com>
2017-09-28 21:44:11 -0500

    Detail track attributes

--- src/components/TrackGrid.jsx
+++ src/components/TrackGrid.jsx
@@ -29,7 +29,7 @@ const tracks = [
 			small: "https://i.scdn.co/image/c4bb8e18b0207c41d9fa1becd4fca1b6f68c06e6",
 			large: "https://i.scdn.co/image/79b9f90d29978a2edd82c8cdbfd3f091207931c7",
 		},
-		duration: 169534,
+		duration: 169,
 		playcount: 1846426,
 		spotifyUri: "spotify:track:2ctvdKmETyOzPb2GiJJT53",
 		albums: "The Darkside of the Moon",
@@ -42,7 +42,7 @@ const tracks = [
 			small: "https://i.scdn.co/image/398054462a0f56f58f05482b9beb3ebfb31cda15",
 			large: "https://i.scdn.co/image/eed15df0d0849836293b9f787fabf077ce9e9f2e",
 		},
-		duration: 341000,
+		duration: 341,
 		playcount: 337458,
 		spotifyUri: "spotify:track:6NNW7XLQ5BecXtPumwkPd5",
 		albums: "Interstellar",
@@ -55,7 +55,7 @@ const tracks = [
 			small: "https://i.scdn.co/image/6196f528b365e8ef504fd0706fb7e87adecfe647",
 			large: "https://i.scdn.co/image/9c2c4a9ac9726bfd996ff96383178bbb5efc59ab",
 		},
-		duration: 258000,
+		duration: 258,
 		playcount: 1711993,
 		spotifyUri: "spotify:track:3RiPr603aXAoi4GHyXx0uy",
 		albums: "A Head Full of Dreams",
@@ -72,7 +72,19 @@ export default class TrackGrid extends React.Component {
 						<Subheader>Tracks</Subheader>
 						{tracks.map(track => (
 							<Link key={track.id} to={"/tracks/" + track.id}>
-								<GridTile title={track.name}>
+								<GridTile
+									title={track.name}
+									subtitle={
+										track.duration +
+										"s long by " +
+										track.artist +
+										". Played " +
+										track.playcount +
+										" times." +
+										" Album: " +
+										track.albums
+									}
+								>
 									<img src={track.imageUrls.large} />
 								</GridTile>
 							</Link>
