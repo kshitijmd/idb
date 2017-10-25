ac305ece45f98f367c4e33d1162eaa5c4a96b003
scottyjackson <scottyjackson97@gmail.com>
2017-10-25 00:41:13 -0500

    fix config stuff

--- application/app/app.py
+++ application/app/app.py
@@ -11,15 +11,18 @@ from api.albums import albums_blueprint
 from api.playlists import playlists_blueprint
 from api.tracks import tracks_blueprint
 
-DEBUG = True
-HOST = '0.0.0.0'
-PORT = 8000
 
 app = Flask(__name__)
-app.config.from_json(os.path.join(os.getcwd(), 'config.json'))
+app.config.from_json(
+    os.path.join(
+        os.path.dirname(os.path.realpath(__file__)),
+        '..',
+        'config.json'
+    )
+)
 db = SQLAlchemy(app)
 
 app.register_blueprint(artists_blueprint, url_prefix='/artists')
 app.register_blueprint(albums_blueprint, url_prefix='/albums')
 app.register_blueprint(playlists_blueprint, url_prefix='/playlists')
-app.register_blueprint(playlists_tracks, url_prefix='/tracks')
+app.register_blueprint(tracks_blueprint, url_prefix='/tracks')
