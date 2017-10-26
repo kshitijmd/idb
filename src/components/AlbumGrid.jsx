083a9882c7d44254b79c4affb3aa54364117c57d
Taylor Kline <taylor.kline@streamingfreebies.com>
2017-10-26 15:57:56 -0500

    Create AlbumGrid based on musicapi response

--- src/components/AlbumGrid.jsx
+++ src/components/AlbumGrid.jsx
@@ -1,83 +1,65 @@
 import React from "react";
-import PageLayout from "./PageLayout";
 import CardGridList from "./CardGridList";
+import ProgressSpinner from "./ProgressSpinner";
+import ErrorCard from "./ErrorCard";
+import * as musicApi from "../services/api/musicApi";
+import * as logger from "../services/logger";
 
-const albums = [
-	{
-		id: 1,
-		name: "The Dark Side of the Moon",
-		artists: [
-			{
-				id: 1,
-				name: "Pink Floyd",
-			},
-		],
-		genres: ["progressive rock"],
-		imageUrls: {
-			small: "https://i.scdn.co/image/c4bb8e18b0207c41d9fa1becd4fca1b6f68c06e6",
-			large: "https://i.scdn.co/image/79b9f90d29978a2edd82c8cdbfd3f091207931c7",
-		},
-		releaseDate: "1973-03-01",
-		playcount: "67392802",
-		spotifyUri: "spotify:artist:0k17h0D3J5VfsdmQ1iZtE9",
-	},
-	{
-		id: 2,
-		name: "Interstellar",
-		artists: [
-			{
-				id: 2,
-				name: "Hans Zimmer",
-			},
-		],
-		genres: ["soundtrack"],
-		imageUrls: {
-			small: "https://i.scdn.co/image/398054462a0f56f58f05482b9beb3ebfb31cda15",
-			large: "https://i.scdn.co/image/eed15df0d0849836293b9f787fabf077ce9e9f2e",
-		},
-		releaseDate: "2014-11-21",
-		playcount: "35284290",
-		spotifyUri: "spotify:artist:0YC192cP3KPCRWx8zr8MfZ",
-	},
-	{
-		id: 3,
-		name: "A Head Full of Dreams",
-		artists: [
-			{
-				id: 3,
-				name: "Coldplay",
-			},
-		],
-		genres: ["rock"],
-		imageUrls: {
-			small: "https://i.scdn.co/image/8a87ac474db652dd6cd9fb208be0f37a4ea9c36b",
-			large: "https://i.scdn.co/image/9c2c4a9ac9726bfd996ff96383178bbb5efc59ab",
-		},
-		releaseDate: "2015-12-04",
-		playcount: "12345678",
-		spotifyUri: "spotify:artist:4gzpq5DPGxSnKTe4SA8HAU",
-	},
-];
+function transformer(album) {
+	return {
+		id: album.id,
+		imageUrl: album.imageUrl,
+		title: album.name,
+		subtitle: album.artist.name,
+		bonusInfo1: `Contains ${album.tracks.length} songs`,
+		bonusInfo2: `Released on ${album.releaseDate}`,
+		bonusInfo3: `Played ${album.playcount} times on Last.FM`,
+	};
+}
 
 export default class AlbumGrid extends React.Component {
-	_transformer = album => ({
-		id: album.id,
-		imageUrl: album.imageUrls.large,
-		title: album.name,
-		subtitle: album.artists[0].name,
-		bonusInfo1: `Released on ${album.releaseDate}`,
-		bonusInfo2: `Played ${album.playcount} times`,
-		bonusInfo3: `Spotify uri: ${album.spotifyUri}`,
-	});
+	state = {
+		loading: true,
+		loaded: false,
+		error: false,
+		data: [],
+	};
+
+	componentDidMount() {
+		musicApi
+			.getAlbums()
+			.then(data => {
+				this.setState({
+					loading: false,
+					loaded: true,
+					error: false,
+					data: data.map(album => transformer(album)),
+				});
+			})
+			.catch(err => {
+				logger.error(err);
+				this.setState({
+					loading: false,
+					loaded: false,
+					error: true,
+					data: [],
+				});
+			});
+	}
+
+	_renderData = () => <CardGridList routerBaseUrl="albums" data={this.state.data} />;
+
+	_renderLoading = () => <ProgressSpinner />;
+
+	_renderError = () => <ErrorCard />;
 
 	render() {
-		return (
-			<PageLayout>
-				<CardGridList
-					routerBaseUrl="albums"
-					data={albums.map(album => this._transformer(album))}
-				/>
-			</PageLayout>
-		);
+		if (this.state.loading) {
+			return this._renderLoading();
+		} else if (this.state.error) {
+			return this._renderError();
+		} else {
+			return this._renderData();
+		}
 	}
 }
