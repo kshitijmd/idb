058fa75a53b9ffddf860d996ac84b579ef6dec40
Taylor Kline <taylor.kline@streamingfreebies.com>
2017-10-26 09:30:31 -0500

    Color entire <body> with lightGrey

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,14 +2,21 @@ import React from "react";
 import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
 import Header from "./Header";
 import Main from "./Main";
+import * as colors from "../constants/colors";
 
-const App = () => (
-	<MuiThemeProvider>
-		<div>
-			<Header />
-			<Main />
-		</div>
-	</MuiThemeProvider>
-);
+export default class App extends React.Component {
+	componentDidMount() {
+		document.body.style.backgroundColor = colors.lightGrey;
+	}
 
-export default App;
+	render() {
+		return (
+			<MuiThemeProvider>
+				<div>
+					<Header />
+					<Main />
+				</div>
+			</MuiThemeProvider>
+		);
+	}
+}
--- webpack.config.js
+++ webpack.config.js
@@ -25,4 +25,5 @@ module.exports = {
 		],
 	},
 	plugins: [],
+   devtool: "source-map",
 };
