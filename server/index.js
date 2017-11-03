47fb036a3cb0ab37dd976beff0fea9edc8b8ed72
Kshitij Delvadiya <kshitij.delvadiya@utexas.edu>
2017-11-03 16:42:00 -0500

    Keep port 9000 for dev

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@
 
 const app = require("./app");
 
-const PORT = process.env.PORT || 80;
+const PORT = process.env.PORT || 9000;
 
 app.listen(PORT, () => {
 	console.log(`App listening on port ${PORT}!`);
