c0c99edaf851d037643bb66bff5b3a9c5a5b28e0
Taylor Kline <taylor.kline@streamingfreebies.com>
2017-11-09 16:04:49 -0600

    Exclude filtering by name

--- src/constants/searchParams.jsx
+++ src/constants/searchParams.jsx
@@ -3,5 +3,5 @@ export const ORDERBY = "order_by";
 export const POP = "popularity";
 export const DESC = "desc";
 export const FILTERBY = "filter_by";
-export const EXCLUDE = "exclude";
+export const EXCLUDE = "notlike";
 export const NAME = "name";
